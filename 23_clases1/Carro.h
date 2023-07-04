dc5ac6a3378e68e403e7c6ff69abbb60e3d6fa3c
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-07-03 18:20:59 -0600

    commit 0307202327388

--- 23_clases1/Carro.h
+++ 23_clases1/Carro.h
@@ -1,1 +1,26 @@
-//clase Carro
+//Ayuda al pre-compilador
+//evita la ambigüedad al hacer include
+//de esta clase desde diferentes archivos
+#ifndef CARRO_H
+#define CARRO_H
+
+#include <iostream>
+using namespace std;
+
+class Carro{
+	private:
+		//ambito privado (private scope)
+		//elementos solo accesibles dentro
+		//de la clase
+	public:
+		//ambito publico (public scope)
+		//elementos accesibles desde cualquier parte
+		//atributos publicos (propiedades)
+		string marca;
+		string modelo;
+		string color;
+		int anio;
+		//constructores
+};
+
+#endif
--- 23_clases1/main.cpp
+++ 23_clases1/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include "Carro.h"
 
 using namespace std;
 
