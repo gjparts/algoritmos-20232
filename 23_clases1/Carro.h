dcb25f93118592389deaf52d30e05a2f6cefcc6d
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-07-05 09:10:22 -0600

    commit 0507202325043

--- 23_clases1/Carro.h
+++ 23_clases1/Carro.h
@@ -21,6 +21,24 @@ class Carro{
 		string color;
 		int anio;
 		//constructores
+		/*
+		Constructor
+		Es un metodo (funcion) que da memoria a cada
+		instancia de la Clase (o sea a cada objeto)
+		-> Se debe llamar igual que la clase
+		-> No tiene tipo de dato puesto que devuelve un puntero de memoria
+		-> siempre va en el ambito publico
+		-> normalmente los usamos para inicializar los atributos de la clase
+		-> en C++ y Java una clase puede tener varios constructores
+		*/
+		//constructor sin parametros
+		Carro(){
+			//inicializar los atributos de esta clase
+			this->marca = "No tiene";
+			this->modelo = "No definido";
+			this->color = "No tiene";
+			this->anio = 0;
+		}
 };
 
 #endif
--- 23_clases1/main.cpp
+++ 23_clases1/main.cpp
@@ -4,6 +4,34 @@
 using namespace std;
 
 int main(){
+	//instanciar un nuevo objeto de clase Carro
+	Carro *c1 = new Carro();
+	cout << "Direccion de memoria de c1: " << c1 << endl;
+	//marca del Carro como venia segun el constructor
+	cout << "Marca de c1: " << c1->marca << endl;
+	
+	//asignar valor a atributo
+	c1->marca = "Toyota";
+	//marca del Carro luego de asignar nuevo valor
+	cout << "Marca de c1: " << c1->marca << endl;
+	
+	cout << "Modelo de c1: " << c1->modelo << endl;
+	cout << "Color de c1: " << c1->color << endl;
+	cout << "Anio de c1: " << c1->anio << endl;
+	
+	//declaramos variable c2 de tipo Carro
+	//pero al no usar el constructor
+	//no es un objeto porque no le dimos memoria
+	//o sea c2 no esta instanciado
+	//c2 tiene memoria CERO por lo tanto es NULL
+	Carro *c2;
+	cout << "Direccion de memoria de c2: " << c2 << endl;
+	
+	//solo si el carro fuera diferente de NULL podemos manipularlo
+	if( c2 != NULL ){
+		c2->marca = "Honda";
+		cout << "Marca de c2: " << c2->marca << endl;
+	}
 	
 	return 123;
 }
